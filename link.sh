cff7ac085a38181be53a44b0a7b46f1fb6cb33bc
Tigran Bantikyan <bantikyan@google.com>
2026-03-23 16:21:21 -0400

    fixed link script with absolute paths; added cp of tmux config

--- link.sh
+++ link.sh
@@ -1,3 +1,6 @@
-ln -sfn zsh-custom ~/.oh-my-zsh/custom
-ln -sfn nvim ~/.config/nvim
-ln -sfn kitty ~/.config/kitty
+ln -sfn ~/dotfiles/zsh-custom ~/.oh-my-zsh/custom
+ln -sfn ~/dotfiles/nvim ~/.config/nvim
+ln -sfn ~/dotfiles/kitty ~/.config/kitty
+
+mkdir -p ~/.config/tmux
+cp ~/dotfiles/tmux.conf ~/.config/tmux
