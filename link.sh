dd47eec126c1ca38d903d7fcf5e4e7d3d198abc9
Tigran Bantikyan <bantikyan@google.com>
2026-04-10 18:47:03 +0000

    improved link.sh robustness

--- link.sh
+++ link.sh
@@ -1,6 +1,20 @@
-ln -sfn ~/dotfiles/zsh-custom ~/.oh-my-zsh/custom
-ln -sfn ~/dotfiles/nvim ~/.config/nvim
-ln -sfn ~/dotfiles/kitty ~/.config/kitty
+#!/bin/bash
+
+link_file() {
+    local src=$1
+    local dst=$2
+
+    if [ -d "$dst" ] && [ ! -L "$dst" ]; then
+        echo "Moving existing directory $dst to ${dst}.bak"
+        mv "$dst" "${dst}.bak"
+    fi
+
+    ln -sfn "$src" "$dst"
+}
+
+link_file ~/dotfiles/zsh-custom ~/.oh-my-zsh/custom
+link_file ~/dotfiles/nvim ~/.config/nvim
+link_file ~/dotfiles/kitty ~/.config/kitty
 
 mkdir -p ~/.config/tmux
-cp ~/dotfiles/tmux.conf ~/.config/tmux
+link_file ~/dotfiles/tmux.conf ~/.config/tmux/tmux.conf
