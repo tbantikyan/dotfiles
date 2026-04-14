8398a7452aa652a6f8118453cbdde4cbb411438e
Tigran Bantikyan <bantikyan@google.com>
2026-04-14 20:43:17 +0000

    added system clipboard yank keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -7,6 +7,8 @@ vim.keymap.set("n", "<C-k>", "<C-y>", { desc = "Scroll up one line" })
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open vim file explorer" })
 
+vim.keymap.set({ "n", "v" }, "<C-y>", '"+y', { desc = "Yank to system clipboard" })
+
 vim.keymap.set("i", "<Tab>", function()
 	return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
 end, { expr = true, silent = true, desc = "Next autocomplete item" })
