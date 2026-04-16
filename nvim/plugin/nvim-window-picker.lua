59d0746418ae115840d29c2442f66410dca499e8
Tigran Bantikyan <59093401+tbantikyan@users.noreply.github.com>
2026-04-16 16:06:13 -0400

    configured window-picker to include terminal buffers

--- nvim/plugin/nvim-window-picker.lua
+++ nvim/plugin/nvim-window-picker.lua
@@ -1,12 +1,22 @@
 vim.pack.add({ "https://github.com/s1n7ax/nvim-window-picker" })
-require 'window-picker'.setup()
+
+local window_picker = require("window-picker")
+window_picker.setup({
+	hint = "floating-big-letter",
+	show_prompt = false,
+	filter_rules = {
+		bo = {
+			-- don't filter out terminal buffers
+			buftype = {},
+		},
+	},
+})
 
 vim.keymap.set("n", "<leader>w", function()
-    local picked_window_id = require('window-picker').pick_window({
-        include_current_win = true,
-        hint = 'floating-big-letter'
-    })
-    if picked_window_id then
-        vim.api.nvim_set_current_win(picked_window_id)
-    end
+	local picked_window_id = window_picker.pick_window({
+		include_current_win = true,
+	})
+	if picked_window_id then
+		vim.api.nvim_set_current_win(picked_window_id)
+	end
 end, { desc = "Pick a window (nvim-window-picker)" })
